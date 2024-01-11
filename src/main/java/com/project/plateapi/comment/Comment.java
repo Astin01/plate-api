5914f3b5cbd079b272dc9406aa093f3723a6aae1
Donghun Won <wonhun1225@gmail.com>
2024-01-11 19:29:35 +0900

    feat(model) : comment
    생성, 수정 날짜 추가
    update 메서드 추가

--- src/main/java/com/project/plateapi/comment/Comment.java
+++ src/main/java/com/project/plateapi/comment/Comment.java
@@ -1,36 +1,56 @@
 package com.project.plateapi.comment;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.project.plateapi.restaurant.Restaurant;
 import com.project.plateapi.user.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Comment {
     @Id
-    @GeneratedValue
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String content;
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String comment;
 
-    @Builder
-    public Comment(String content){
-        this.content=content;
-    }
+    @Column(name = "created_date")
+    @CreatedDate
+    private String createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private String modifiedDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    public void update(String comment, String modifiedDate) {
+        this.comment = comment;
+        this.modifiedDate = modifiedDate;
+    }
 }
