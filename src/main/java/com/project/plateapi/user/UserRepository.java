3ac25a64ac9c23e686017d069290eb3d913777de
Donghun Won <wonhun1225@gmail.com>
2024-01-19 20:01:31 +0900

    feat(repository) : user 메서드 추가
    이름으로 찾기
    id로 찾기 메서드 추가

--- src/main/java/com/project/plateapi/user/UserRepository.java
+++ src/main/java/com/project/plateapi/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.project.plateapi.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByNickname(String nickname);
 
+    Optional<User> findById(Long id);
 }
