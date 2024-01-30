114d4f520d920a3e5ec77a3e9361cedb3d38939a
Donghun Won <wonhun1225@gmail.com>
2024-01-30 21:26:03 +0900

    feat(dto) : 유저 정보 반환 dto 추가

---
+++ src/main/java/com/project/plateapi/user/dto/UserInfoResponseDto.java
@@ -0,0 +1,42 @@
+package com.project.plateapi.user.dto;
+
+import com.project.plateapi.role.Role;
+import com.project.plateapi.user.Users;
+import com.project.plateapi.user_role.UserRole;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserInfoResponseDto {
+    private Long id;
+    private String userId;
+    private String name;
+    private String nickname;
+    private String email;
+    private UserRoleDto userRoles;
+
+    public UserInfoResponseDto(Users user){
+        this.id = user.getId();
+        this.userId = user.getUserId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.userRoles = new UserRoleDto(user.getUserRoles());
+    }
+    @Getter
+    public static class UserRoleDto {
+        private final List<Role> userRole;
+        public UserRoleDto(List<UserRole> userRoleLists) {
+            this.userRole = userRoleLists.stream().map(role->role.getRole()).collect(Collectors.toList());
+        }
+    }
+
+}
