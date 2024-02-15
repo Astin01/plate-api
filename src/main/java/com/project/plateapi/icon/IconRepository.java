eb0348e41ba7c48b61797cd5c24488d7c880f445
Donghun Won <wonhun1225@gmail.com>
2024-02-15 20:45:47 +0900

    feat(repository) : IconRepository

---
+++ src/main/java/com/project/plateapi/icon/IconRepository.java
@@ -0,0 +1,9 @@
+package com.project.plateapi.icon;
+
+import com.project.plateapi.restaurant.Restaurant;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IconRepository extends JpaRepository<Icon, Long> {
+}
