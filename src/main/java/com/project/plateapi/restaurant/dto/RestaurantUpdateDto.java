7dcc96235c73c062f6e391894ee3c00640f036d3
Donghun Won <wonhun1225@gmail.com>
2024-02-17 20:47:19 +0900

    refactor(dto) : naming 변경

--- src/main/java/com/project/plateapi/restaurant/dto/RestaurantUpdateDto.java
+++ src/main/java/com/project/plateapi/restaurant/dto/RestaurantUpdateDto.java
@@ -1,15 +1,12 @@
 package com.project.plateapi.restaurant.dto;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Setter(AccessLevel.NONE)
-public class RestaurantUpdateRequestDto {
+public class RestaurantUpdateDto {
     private String name;
     private String category;
     private String icon;
