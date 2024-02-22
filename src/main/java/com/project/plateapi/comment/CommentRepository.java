6a443329857d35d131bafbcf752b124dd5ff5346
Donghun Won <wonhun1225@gmail.com>
2024-02-22 21:26:44 +0900

    feat(repository) : CommentRepository

--- src/main/java/com/project/plateapi/comment/CommentRepository.java
+++ src/main/java/com/project/plateapi/comment/CommentRepository.java
@@ -1,9 +1,10 @@
 package com.project.plateapi.comment;
 
+import com.project.plateapi.discussion.Discussion;
 import com.project.plateapi.restaurant.Restaurant;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    List<Comment> findAllByDiscussion(Discussion discussion);
 }
