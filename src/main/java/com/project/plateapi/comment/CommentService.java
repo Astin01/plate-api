2b65b1e955d37240929c0040fd0e0c64c1041252
Donghun Won <wonhun1225@gmail.com>
2024-02-22 21:25:24 +0900

    feat(service)  : CommentService
    댓글 전부 찾아서 반환

--- src/main/java/com/project/plateapi/comment/CommentService.java
+++ src/main/java/com/project/plateapi/comment/CommentService.java
@@ -1,6 +1,7 @@
 package com.project.plateapi.comment;
 
 import com.project.plateapi.comment.dto.CommentCreateDto;
+import com.project.plateapi.comment.dto.CommentResponseDto;
 import com.project.plateapi.comment.dto.CommentUpdateDto;
 import com.project.plateapi.discussion.Discussion;
 import com.project.plateapi.discussion.DiscussionRepository;
@@ -8,6 +9,7 @@ import com.project.plateapi.security.custom.dto.CustomUser;
 import com.project.plateapi.user.Users;
 import com.project.plateapi.user.UserRepository;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
-    private final UserRepository userRepository;
     private final DiscussionRepository discussionRepository;
 
 
@@ -50,4 +51,13 @@ public class CommentService {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    public ResponseEntity<?> getComment(long discussion_id) {
+        Discussion discussion = discussionRepository.findById(discussion_id).orElseThrow(IllegalArgumentException::new);
+
+        List<Comment> commentList = commentRepository.findAllByDiscussion(discussion);
+
+        CommentResponseDto responseDto = new CommentResponseDto(commentList);
+
+        return new ResponseEntity<>(responseDto,HttpStatus.OK);
+    }
 }
