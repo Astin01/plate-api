bc8a18845d913d32529605de64801afedb1deaf0
Donghun Won <wonhun1225@gmail.com>
2024-03-02 18:52:07 +0900

    refactor(repository) : 쿼리 속도 개선

--- src/main/java/com/project/plateapi/suggestion/SuggestionRepository.java
+++ src/main/java/com/project/plateapi/suggestion/SuggestionRepository.java
@@ -3,8 +3,13 @@ package com.project.plateapi.suggestion;
 import com.project.plateapi.restaurant.Restaurant;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SuggestionRepository extends JpaRepository<Suggestion, Long> {
     Optional<Suggestion> findById(Long id);
+
+    @EntityGraph(attributePaths = {"user","restaurant"})
+    List<Suggestion> findAll();
 }
