4aab6e287b252af6425e0057e5314a3b51ef0b9b
Donghun Won <wonhun1225@gmail.com>
2024-03-29 17:40:37 +0900

    refactor(controller) : 반환값 명료화

--- src/main/java/com/project/plateapi/comment/controller/CommentController.java
+++ src/main/java/com/project/plateapi/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.project.plateapi.comment.controller;
 import com.project.plateapi.comment.service.CommentService;
 import com.project.plateapi.comment.controller.dto.request.CommentCreateDto;
 import com.project.plateapi.comment.controller.dto.request.CommentUpdateDto;
+import com.project.plateapi.comment.service.dto.response.CommentResponseDto;
 import com.project.plateapi.security.custom.dto.CustomUser;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/comment")
 @RestController
 public class CommentController {
     private final CommentService service;
@@ -23,23 +26,23 @@ public class CommentController {
         this.service = service;
     }
 
-    @GetMapping("/api/comment/{discussion_id}")
-    public ResponseEntity<?> getComment(@PathVariable long discussion_id) {
+    @GetMapping("/{discussion_id}")
+    public ResponseEntity<CommentResponseDto> getComment(@PathVariable long discussion_id) {
         return service.getComment(discussion_id);
     }
 
-    @PostMapping("/api/comment/{discussion_id}")
-    public ResponseEntity<?> createComment(@AuthenticationPrincipal CustomUser customUser, @PathVariable long discussion_id, @Valid @RequestBody CommentCreateDto createDto) {
+    @PostMapping("/{discussion_id}")
+    public ResponseEntity<Void> createComment(@AuthenticationPrincipal CustomUser customUser, @PathVariable long discussion_id, @Valid @RequestBody CommentCreateDto createDto) {
         return service.createComment(customUser, discussion_id, createDto);
     }
 
-    @PutMapping("/api/comment/{comment_id}")
-    public ResponseEntity<?> updateComment(@PathVariable long comment_id, @RequestBody CommentUpdateDto updateDto) {
+    @PutMapping("/{comment_id}")
+    public ResponseEntity<Void> updateComment(@PathVariable long comment_id, @RequestBody CommentUpdateDto updateDto) {
         return service.updateComment(comment_id, updateDto);
     }
 
-    @DeleteMapping("/api/comment/{comment_id}")
-    public ResponseEntity<?> deleteComment(@PathVariable long comment_id) {
+    @DeleteMapping("/{comment_id}")
+    public ResponseEntity<Void> deleteComment(@PathVariable long comment_id) {
         return service.deleteComment(comment_id);
     }
 
