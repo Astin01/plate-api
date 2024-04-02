45aefdbacdd116b140b4beb400787e45a2459acc
Donghun Won <wonhun1225@gmail.com>
2024-04-02 16:11:17 +0900

    refactor(dto) : dto를 record로 변경

--- src/main/java/com/project/plateapi/comment/controller/dto/request/CommentUpdateDto.java
+++ src/main/java/com/project/plateapi/comment/controller/dto/request/CommentUpdateDto.java
@@ -1,35 +1,6 @@
 package com.project.plateapi.comment.controller.dto.request;
 
-import com.project.plateapi.comment.domain.Comment;
-import com.project.plateapi.discussion.domain.Discussion;
-import com.project.plateapi.user.domain.Users;
 import jakarta.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class CommentUpdateDto {
-    @NotBlank
-    private String comment;
-    private final String modifiedDate= LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-    private Users user;
-    private Discussion discussion;
-
-    public Comment toEntity() {
-        return Comment.builder()
-                .comment(comment)
-                .modifiedDate(modifiedDate)
-                .user(user)
-                .discussion(discussion)
-                .build();
-    }
-
-
+public record CommentUpdateDto(@NotBlank String comment) {
 }
