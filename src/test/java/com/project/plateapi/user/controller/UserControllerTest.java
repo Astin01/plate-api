2f0baa61af71f0c61f634b98f9a0aa412fbba709
Donghun Won <wonhun1225@gmail.com>
2024-04-25 22:05:46 +0900

    test : UserControllerTest 추가
    유저 정보수정 테스트
    유저 삭제 테스트

--- src/test/java/com/project/plateapi/user/controller/UserControllerTest.java
+++ src/test/java/com/project/plateapi/user/controller/UserControllerTest.java
@@ -1,5 +1,11 @@
 package com.project.plateapi.user.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -7,16 +13,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.plateapi.role.domain.Role;
 import com.project.plateapi.security.custom.dto.CustomUser;
 import com.project.plateapi.user.controller.dto.request.UserInfoRequest;
+import com.project.plateapi.user.domain.UserRepository;
 import com.project.plateapi.user.domain.Users;
-import com.project.plateapi.user.service.UserService;
 import com.project.plateapi.user_role.domain.UserRole;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -25,7 +34,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @AutoConfigureMockMvc
 @Transactional
@@ -39,6 +47,7 @@ class UserControllerTest {
     private static final String EMAIL ="ace1225@naver.com";
     private static final String BASE_URL = "/api/users";
 
+
     @Autowired
     MockMvc mockMvc;
 
@@ -46,14 +55,15 @@ class UserControllerTest {
     ObjectMapper objectMapper;
 
     @Autowired
-    UserService userService;
-
-    @Autowired
     PasswordEncoder passwordEncoder;
+
+    @MockBean
+    UserRepository userRepository;
 
     @BeforeEach
     public void setUp() {
         Users user = Users.builder()
+                .id(1L)
                 .userId(ID)
                 .userPassword(passwordEncoder.encode(PASSWORD))
                 .name(NAME)
@@ -76,7 +86,7 @@ class UserControllerTest {
 
         SecurityContextHolder.getContext().setAuthentication(auth);
     }
-
+    @DisplayName("회원가입이 정상적으로 되는지 테스트한다")
     @Test
     void signUp() throws Exception {
         String content = objectMapper.writeValueAsString(new UserInfoRequest(ID,PASSWORD,NAME,NICKNAME,EMAIL));
@@ -85,21 +95,66 @@ class UserControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(status().isOk());
+                .andExpect(status().is(HttpStatus.CREATED.value()));
     }
 
+    @DisplayName("유저 정보를 조회할 수 있는지 테스트한다")
     @Test
     void getUserInfo() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL+"/info")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
         )
-                .andExpect(status().isOk())
+                .andExpect(status().is(HttpStatus.OK.value()))
                 .andExpect(jsonPath("$.userId").value(ID))
                 .andExpect(jsonPath("$.name").value(NAME))
                 .andExpect(jsonPath("$.nickname").value(NICKNAME))
                 .andExpect(jsonPath("$.email").value(EMAIL))
                 .andExpect(jsonPath("$.userRoles.userRole[0].role").value("USER"));
     }
+
+    @DisplayName("유저 정보가 수정되는지 테스트한다")
+    @Test
+    void changeUserInfo() throws Exception {
+        String changedName = "홍길동";
+        String changedPw = "ace1234";
+        String changedNickName = "은하철도999";
+        String changeEmail ="rail999@gmail.com";
+        String content = objectMapper.writeValueAsString(new UserInfoRequest(ID,changedPw,changedName,changedNickName,changeEmail));
+        Users user = Users.builder()
+                .id(1L)
+                .userId(ID)
+                .userPassword(passwordEncoder.encode(PASSWORD))
+                .name(NAME)
+                .nickname(NICKNAME)
+                .email(EMAIL)
+                .build();
+        when(userRepository.findByUserId(ID)).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.put(BASE_URL)
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().is(HttpStatus.OK.value()));
+
+        verify(userRepository, times(1)).findByUserId(ID);
+    }
+
+    @DisplayName("유저가 삭제되는지 테스트한다")
+    @Test
+    void deleteUser() throws Exception {
+        doNothing().when(userRepository).deleteByUserId(ID);
+        doThrow(new IllegalArgumentException("Unable to delete user")).when(userRepository).delete(any(Users.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(BASE_URL+"/"+ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().is(HttpStatus.NO_CONTENT.value()));
+
+        verify(userRepository, times(1)).deleteByUserId(ID);
+    }
+
 
 }
