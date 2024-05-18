553fb7e00d64190debce05f374471ca5b5bd64ed
Donghun Won <wonhun1225@gmail.com>
2024-05-18 21:35:31 +0900

    feat(repository) : IconRepository 아이디로 찾기 메서드 추가

--- src/main/java/com/project/plateapi/icon/domain/IconRepository.java
+++ src/main/java/com/project/plateapi/icon/domain/IconRepository.java
@@ -1,6 +1,9 @@
 package com.project.plateapi.icon.domain;
 
+import com.project.plateapi.notice.domain.Notice;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IconRepository extends JpaRepository<Icon, Long> {
+    Optional<Icon> findById(Long id);
 }
