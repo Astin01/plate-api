e312b5ecaf89bd513b7e9d4cc83904054744e898
Donghun Won <wonhun1225@gmail.com>
2024-05-18 21:34:21 +0900

    feat(service) : IconService
    아이디로 아이콘 찾기
    아이콘 생성
    아이콘 수정
    아이콘 삭제

--- src/main/java/com/project/plateapi/icon/service/IconService.java
+++ src/main/java/com/project/plateapi/icon/service/IconService.java
@@ -1,23 +1,59 @@
 package com.project.plateapi.icon.service;
 
+import com.project.plateapi.icon.controller.dto.request.IconRequestDto;
 import com.project.plateapi.icon.domain.Icon;
 import com.project.plateapi.icon.domain.IconRepository;
+import com.project.plateapi.icon.service.dto.response.IconListResponseDto;
 import com.project.plateapi.icon.service.dto.response.IconResponseDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class IconService {
     private final IconRepository iconRepository;
 
-    public IconResponseDto findAllIcon() {
+    public IconResponseDto findIcon(Long iconId) {
+        Icon icon = iconRepository.findById(iconId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        return new IconResponseDto(icon);
+    }
+
+    public IconListResponseDto findAllIcon() {
         List<Icon> icons = iconRepository.findAll();
 
-        return new IconResponseDto(icons);
+        return new IconListResponseDto(icons);
     }
+
+    public Long createIcon(IconRequestDto request) {
+        Icon icon = Icon.builder()
+                .icon(request.icon())
+                .name(request.name())
+                .link(request.link())
+                .build();
+
+        Icon savedIcon = iconRepository.save(icon);
+
+        return savedIcon.getId();
+
+    }
+
+    @Transactional
+    public void editIcon(Long iconId, IconRequestDto request) {
+        Icon icon = iconRepository.findById(iconId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        icon.update(request.icon(), request.name(), request.link());
+    }
+
+    @Transactional
+    public void deleteIcon(Long iconId) {
+        iconRepository.deleteById(iconId);
+    }
+
 
 }
